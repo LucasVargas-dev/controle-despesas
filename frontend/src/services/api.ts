834d3e4d00c629dc965d5c7d7e4218394cd95878
Lucas Vargas <lucas.vargas@universo.univates.br>
2022-06-04 14:41:55 -0300

    Add heroku

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001',
+  //baseURL: 'http://localhost:3001',
+  baseURL: process.env.VITE_API_URL,
 });
 
 api.interceptors.response.use(
