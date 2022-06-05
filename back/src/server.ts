ea7b23b452c5af99be1eb3439ff13b7dcfaf28dc
Lucas Vargas <lucas.vargas@universo.univates.br>
2022-06-05 11:17:32 -0300

    Adiciona visualização da porta no server.

--- back/src/server.ts
+++ back/src/server.ts
@@ -14,4 +14,4 @@ app.use(express.json());
 app.use(cors())
 app.use(routes);
 
-app.listen(port, () => console.log("Server is running!"));
+app.listen(port, () => console.log(`Server is running! Porta: ${port}`));
